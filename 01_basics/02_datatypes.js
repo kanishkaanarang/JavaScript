c0012b2e28ec5cceea15084effcbc73537544379
Kanishka Narang <kanishkaanarang@gmail.com>
2025-05-04 12:49:54 +0000

    learnt datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // code is treated as new version, even tho it is not necerssary but to let others know
+
+// alert(3+3) // can be used in browser directly but not here because we are using nodeJS
+
+// code readabilty should be high
+
+// PRIMITIVE DATATYPES :
+
+let name= "kanishka" //string datatype
+
+let age= 19 //number {range => 2^53} //bigInt if the number is super big
+
+let isLoggedIn= false //boolean {true,false}
+
+//null => standalone value, representation of empty value
+// undefined => when the value is not defined
+//symbol => used for uniqueness
+
+console.log(typeof "Kanishka");
+console.log(typeof age);
+console.log(typeof null); //gives object which is a non-primitive datatype
+console.log(typeof undefined); //gives undefined
