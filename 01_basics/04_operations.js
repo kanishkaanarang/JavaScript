32773c98ec7a6a0f7a081fca06523466e5dd92e6
Kanishka Narang <kanishkaanarang@gmail.com>
2025-05-06 05:52:33 +0000

    about operations

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,34 @@
+let value = 3
+let negVlue= -value
+// console.log(negVlue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2/2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2%2); => 0
+
+let str1 = "hello "
+let str2 = "kanishka"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log(1+"2")
+console.log("1"+2) // both gives 12
+console.log("1"+2+2) // gives 122 
+console.log(1+2+"2") //gives 32
+// but the upper code is never suggested
+
+
+console.log(+true) //gives 1 
+console.log(+"")// gives 0
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+
+let gameCount=100
+gameCount++ // postfix operator
+++gameCount // prefix operator
+console.log(gameCount);
