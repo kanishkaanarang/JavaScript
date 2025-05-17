b61fbf1fd37dcb4965068d291fb11ef7bb93d733
Kanishka Narang <kanishkaanarang@gmail.com>
2025-05-17 19:21:54 +0000

    while & do while

---
+++ 05_iterations/two.js
@@ -0,0 +1,25 @@
+// while loop
+
+let index =0
+while (index<=10) {
+    console.log(`value of index is ${index}`)
+    index += 2
+}
+
+let myArray= ["kanishka","nisha","naveen","ishaan"]
+let arr=0
+while (arr< myArray.length) {
+    console.log(`value is ${myArray[arr]}`)
+    arr++;
+}
+
+// do while
+
+let score = 11
+do {
+    console.log(`score is ${score}`)
+    score ++
+} while (score<=10);
+
+// do while : first executes then checks
+// while : first checks condition then executes
